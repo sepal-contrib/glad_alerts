0038af7ea635c36f55487d481aff614752384f24
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-07-07 15:01:58 +0000

    add atributions on the map

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -30,6 +30,7 @@ def init_map():
     m.add_basemap('CartoDB.Positron')
     m.add_control(geemap.ZoomControl(position='topright'))
     m.add_control(geemap.LayersControl(position='topright'))
+    m.add_control(geemap.AttributionControl(position='bottomright'))
     m.add_control(geemap.ScaleControl(position='bottomleft', imperial=False))
     
     return (dc, m)
